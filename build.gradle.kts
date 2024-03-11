babfce51b1435fee9a9f3d6e1473082211e5eb7f
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-11 16:08:22 +0200

    Add ktor http client and junit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,24 @@ plugins {
 group = "com.cybercube"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
 }
 
+val ktorVersion = "2.3.9"
+
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("io.ktor:ktor-client-core:${ktorVersion}")
+    implementation("io.ktor:ktor-client-logging:${ktorVersion}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
+    implementation("io.ktor:ktor-client-apache5:${ktorVersion}")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
